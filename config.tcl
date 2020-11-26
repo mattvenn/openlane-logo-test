c24d1f6df5d46afb30943027a9388a4dd3002c59
Matt Venn <matt@mattvenn.net>
2020-11-26 19:55:16 +0100

    more futzing

--- config.tcl
+++ config.tcl
@@ -10,13 +10,14 @@ set ::env(PDN_CFG) $::env(DESIGN_DIR)/pdn.tcl
 # turn off clock
 #set ::env(CLOCK_TREE_SYNTH) 0
 #set ::env(CLOCK_PORT) ""
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 2000 2000"
 
 set ::env(CLOCK_PERIOD) "100"
 set ::env(CLOCK_PORT) "clk"
-set ::env(CLOCK_TREE_SYNTH) 0
 
 set ::env(FP_CORE_UTIL) 5;                                                                   
-set ::env(PL_TARGET_DENSITY) 0.5 
+set ::env(PL_TARGET_DENSITY) 0.01 
 
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
